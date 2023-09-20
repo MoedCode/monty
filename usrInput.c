636569c3091065d5a728d73edfa2626a4762f485
MoedCode <mohamed71291@gmail.com>
2023-09-20 23:29:51 +0300

    codin..command arg files handled (automated command)

--- usrInput.c
+++ usrInput.c
@@ -12,12 +12,13 @@ size_t Get_Inout(char *filePath)
 	FILE *file = fopen(filePath, "r");
 
 	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filePath);
 		exit(EXIT_FAILURE);
-
-	printf("filePath : %s\n",filePath);
+	}
 	while (status != -1 )
 	{
-		status = getline(filePath, &size, file);
+		status = getline(&command, &size, file);
 		args = Tokenize_in_Arr(command, " \n");
 		Print_2D_Arr(args, 0);
 		FreeVar(command);
