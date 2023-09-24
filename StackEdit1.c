71fbf07d22e6d99c9277cf08c3907d34f9d86771
MoedCode <mohamed71291@gmail.com>
2023-09-24 20:33:03 +0300

    ==13705== ERROR SUMMARY: 2 errors from 2 contexts (suppressed: 0 from 0) (automated command)

--- StackEdit1.c
+++ StackEdit1.c
@@ -24,11 +24,10 @@ void comments(stack_t **stack, unsigned int line_number)
 }
 
 
-
 /**
- * pchar - Prints the ASCII  value at the top of the stack, followed by a new line.
+ * pchar - Prints the ASCII value at the top of the stack as a character, followed by a new line.
  * @stack: A pointer to the head of the stack.
- * @line_number: The line number in the Monty byte code file.
+ * @line_number: The line number in the Monty bytecode file.
  */
 void pchar(stack_t **stack, unsigned int line_number)
 {
@@ -37,9 +36,15 @@ void pchar(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
 		EXIT(stack);
 	}
-	if ((*stack)->n > 127)
+	if ((*stack)->n < 0 || (*stack)->n > 127)
+	{
 		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		EXIT(stack);
+	}
 
 	putchar((*stack)->n);
-	putchar(10);
+	putchar('\n');
+
+	/* Pop the top element from the stack */
+	pop(stack, line_number);
 }
