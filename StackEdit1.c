1cf9331536e826d3e6fdea72149c06678b25edd0
MoedCode <mohamed71291@gmail.com>
2023-09-24 21:31:35 +0300

    ==13705== ERROR SUMMARY: 2 errors from 2 contexts (suppressed: 0 from 0) (automated command)

--- StackEdit1.c
+++ StackEdit1.c
@@ -37,7 +37,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
 		EXIT(stack);
 	}
-	if ((*stack)->n > 127)
+	if ((*stack)->n < 0 &&(*stack)->n > 127)
 	{
 		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
 		EXIT(stack);
